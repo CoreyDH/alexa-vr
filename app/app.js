3eec3c9513f9c5d53a86649f334f212e98f55e94
Matt Davis <matt_davis@me.com>
2017-01-11 19:57:45 -0600

    old project cleanup

--- app/app.js
+++ app/app.js
@@ -3,17 +3,9 @@
 import React    from 'react'
 import ReactDOM from 'react-dom'
 
-import Header   from './components/Header'
-import Content  from './components/Content'
-import Footer   from './components/Footer'
-
 // Render Main Component
 ReactDOM.render(
   <div className='main-container'>
-    
-    <Header />
-    <Content />
-
   </div>,
   document.getElementById('app')
 )
