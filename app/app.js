3c65c92ec1c2e4d50848792aab5eea722bbdc871
Matt Davis <matt_davis@me.com>
2017-01-29 13:31:18 -0600

    new Pikachu

--- app/app.js
+++ app/app.js
@@ -1,9 +1,14 @@
 'use strict';
 
-import 'aframe';
-import {Entity, Scene} from 'aframe-react';
+import 'aframe'
+import extras   from 'aframe-extras'
+import {Entity, Scene} from 'aframe-react'
 import React    from 'react'
 import ReactDOM from 'react-dom'
+
+extras.loaders.registerAll();
+
+// AFRAME.registerComponent()
 
 // Render Main Component
 ReactDOM.render(
@@ -16,19 +21,16 @@ ReactDOM.render(
         <a-asset-item id="boar-obj" src="/assets/obj/boar-obj.obj"></a-asset-item>
         <a-asset-item id="boar-mtl" src="/assets/obj/boar-obj.mtl"></a-asset-item>
 
-        <a-asset-item id="deer-obj" src="/assets/obj/deer-obj.obj"></a-asset-item>
-        <a-asset-item id="deer-mtl" src="/assets/obj/deer-obj.mtl"></a-asset-item>
-
         <a-asset-item id="wolf-obj" src="/assets/obj/wolf-obj.obj"></a-asset-item>
         <a-asset-item id="wolf-mtl" src="/assets/obj/wolf-obj.mtl"></a-asset-item>
       </a-assets>
       
       <Entity geometry={{primitive: 'plane', width: 100, height: 100}} material="color: #7BC8A4" position="0 0 -4" rotation="-90 0 0"/>
       
-      <Entity obj-model="obj: #bear-obj; mtl: #bear-mtl" position="0 0 -5"   rotation="0 0 0"  scale=".1 .1 .1" />
-      <Entity obj-model="obj: #boar-obj; mtl: #boar-mtl" position="3 0.5 -4" rotation="0 0 0"  scale=".1 .1 .1" />
-      <Entity obj-model="obj: #deer-obj; mtl: #deer-mtl" position="-4 0 -5"  rotation="0 90 0" scale=".1 .1 .1" />
+      <Entity obj-model="obj: #bear-obj; mtl: #bear-mtl" position="0 0 -5"    rotation="0 0 0"  scale=".1 .1 .1" />
+      <Entity obj-model="obj: #boar-obj; mtl: #boar-mtl" position="3 0.5 -4"  rotation="0 0 0"  scale=".1 .1 .1" />
       <Entity obj-model="obj: #wolf-obj; mtl: #wolf-mtl" position="-2 0.5 -3" rotation="0 0 0"  scale=".1 .1 .1" />
+      <Entity position="-2 0 -5" scale=".05 .05 .05" object-model="src: url(/assets/obj/pikachu/pikachu.json);" />
 
     </Scene>
   </div>,
--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,18 @@ module.exports = {
     filename: "public/assets/js/bundle.js"
   },
   module: {
-    noParse: [
-      /node_modules\/aframe\/dist\/aframe.js/,
-    ],
     loaders: [
       {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
         loader: 'babel',
         query: {
-          presets: ['react', 'es2015']
+          presets: ['react', 'es2015', 'stage-0']
         }
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
       }
     ]
   },
