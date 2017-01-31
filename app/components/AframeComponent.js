f3f1f404a67ad7f91ad09adca72adac38d4be88f
Corey H <rewer6@gmail.com>
2017-01-30 19:40:01 -0600

    preping for merge

--- app/components/AframeComponent.js
+++ app/components/AframeComponent.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
-import Header from "./Header";
-
-export default class Layout extends React.Component {
+export default class AframeComponent extends React.Component {
     render() {
         return (
             <div>
