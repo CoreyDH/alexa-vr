1f6de78e736e7fb68118ad6bcfde35cc1f17c278
Matt Davis <matt_davis@me.com>
2017-01-31 17:19:52 -0600

    battle text added

--- app/components/Aframe.js
+++ app/components/Aframe.js
@@ -2,8 +2,13 @@ import React from 'react'
 import axios from 'axios'
 
 import 'aframe'
-import extras from 'aframe-extras'
 import { Entity, Scene } from 'aframe-react'
+import extras from 'aframe-extras'
+import 'aframe-bmfont-text-component'
+
+import Camera from './aframe/Camera'
+import Text from './aframe/Text'
+import Sky from './aframe/Sky'
 
 import * as game from '../../helpers/game.js'
 
@@ -11,37 +16,97 @@ extras.loaders.registerAll();
 const socket = io();
 
 export default class Aframe extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            player: {
+                name: 'Loading',
+                hp: 0
+            },
+            playerMaxHP: 0,
+            cpu: {
+                name: 'Loading',
+                hp: 0
+            },
+            cpuMaxHP: 0
+        };
+    }
+    
     componentWillMount() {
-        socket.on('attack', data => {
-            const attacker = this.state.player,
-                  defender = this.state.cpu,
-                  move     = this.state.player[data.move],
-                  damage   = game.getDamage(attacker, defender, move);
-
-            defender.hp -= damage;
-            this.setState({ cpu: defender });
-
-            console.log(`${attacker.name} attacks for ${damage} damage`);
-            console.log(`${defender.name} has ${this.state.cpu.hp} HP remaining`);
-            //   document.getElementById('alexa').emit(data.move);
-        });
-
+        // Get pet data and set state
         axios.get('/pets').then(res => {
             this.setState({
-                player: res.data[1],
-                cpu:    res.data[0]
+                player:      res.data[1],
+                playerMaxHP: res.data[1].hp,
+                cpu:         res.data[0],
+                cpuMaxHP:    res.data[0].hp
             });
             console.log(`Player: ${this.state.player.name}, CPU: ${this.state.cpu.name}`);
         });
+
+        // On player attack
+        socket.on('attack', data => {
+            this.setState(game.playerAttack(this.state, data.move));
+            setTimeout(() => this.setState(game.cpuAttack(this.state, data.move)), 2000);
+            document.getElementById('player').emit(data.move);
+        });
+
     }
 
     render() {
         return (
             <Scene>
-                <Entity geometry={{primitive: 'plane', width: 100, height: 100}} material="color: #55BB88" position="0 0 -4" rotation="-90 0 0"/>
-                <a-sky color="#F0FEFE"></a-sky>
+                <a-sky
+                    id="sky"
+                    color="#F0FEFE"
+                />
+                <Entity
+                    id="ground"
+                    geometry={{primitive: 'plane', width: 100, height: 100}}
+                    material="color: #55BB88"
+                    position="0 0 -4"
+                    rotation="-90 0 0"
+                />
                 
-                <Entity position="0 0 -5" rotation="5 0 2" scale=".05 .05 .05" object-model="src: url(/assets/obj/pikachu/pikachu.json);" />
+                <a-cylinder
+                    id="player"
+                    position="2.08 0.75 -2.87"
+                    radius="0.2"
+                    height="1.0"
+                    color="#222222"
+                >
+                    <a-animation
+                        attribute="rotation"
+                        dur="5000"
+                        fill="forwards"
+                        to="180 360 0"
+                        begin="dance"
+                    ></a-animation>
+                </a-cylinder>
+                <Text
+                    id="player_name"
+                    text={`${this.state.player.name}: ${this.state.player.hp} / ${this.state.playerMaxHP}`}
+                    color="#DADADA"
+                    position="1.70 0.06 -2.51"
+                />
+
+                <Entity
+                    id="cpu"
+                    position="-6.23 0.00 -7.80"
+                    rotation="5.16 71.05 1.72"
+                    scale=".05 .05 .05"
+                    object-model="src: url(/assets/obj/pikachu/pikachu.json);"
+                />
+                <Text
+                    id="cpu_name"
+                    text={`${this.state.cpu.name}: ${this.state.cpu.hp} / ${this.state.cpuMaxHP}`}
+                    color="#DADADA"
+                    position="-4.81 0.04 -5.53"
+                    scale="2.00 2.00 2.00"
+                />
+                
+
             </Scene>
         );
     }
--- helpers/game.js
+++ helpers/game.js
@@ -1,15 +1,46 @@
 'use strict';
 
+// Level constants
 const LEVEL     = 20,
       LEVEL_MOD = ((2 * LEVEL) + 10) / 250;
 
-function modifier () { return ((Math.random() / 6.666666666) + .85) }
+// Damage calculation
+function getDamage (attacker, defender, move) {
+    
+    // Random damage modifier
+    function modifier () { return ((Math.random() / 6.666666667) + .85) }
+
+    return Math.round((LEVEL_MOD * (attacker.attack / defender.defense) * move.power + 2) * modifier());
+}
+
 
 module.exports = {
-    getDamage: function(attacker, defender, move) {
-        return Math.round((LEVEL_MOD * (attacker.attack / defender.defense) * move.power + 2) * modifier());
-    },
-    getSpecialDamage: function() {
+    playerAttack: function(state, moveName) {
+        const attacker = state.player,
+              defender = state.cpu,
+              move     = state.player[moveName],
+              damage   = getDamage(attacker, defender, move);
+        
+        state.cpu.hp - damage < 0 ? state.cpu.hp = 0 : state.cpu.hp -= damage;
 
+        console.log(`${attacker.name} uses ${move.name} and does ${damage} damage`);
+        console.log(`${defender.name} has ${state.cpu.hp} HP remaining`);
+        if (state.cpu.hp === 0) console.log(`${defender.name} has fainted!`)
+
+        return state;
+    },
+    cpuAttack: function(state) {
+        const attacker = state.cpu,
+              defender = state.player,
+              move     = state.cpu['move' + Math.ceil(Math.random() * 4)],
+              damage   = getDamage(attacker, defender, move);
+        
+        state.player.hp - damage < 0 ? state.player.hp = 0 : state.player.hp -= damage;
+
+        console.log(`${attacker.name} uses ${move.name} and does ${damage} damage`);
+        console.log(`${defender.name} has ${state.player.hp} HP remaining`);
+        if (state.player.hp === 0) console.log(`${defender.name} has fainted!`)
+
+        return state;
     }
 }
